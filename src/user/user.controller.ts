3b8d69dd7a13066eda9d9261aa377f80e1c1a95c
Francisco Hauva <phauva@gmail.com>
2022-12-03 23:01:44 -0300

    Two different endpoints for users.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,12 +6,21 @@ import {
 import { User } from '@prisma/client';
 import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
+import { UserService } from './user.service';
 
 @UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
+  constructor(private userService: UserService) {}
   @Get('me')
   getMe(@GetUser() user: User) {
     return user;
+  }
+
+  @Get('me/full')
+  getMeFull(@GetUser('id') userId: number) {
+    return this.userService.getUserInfoById(
+      userId,
+    );
   }
 }
---
+++ src/user/user.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as _ from 'lodash';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+  async getUserInfoById(userId: number) {
+    const user =
+      await this.prisma.user.findUnique({
+        where: {
+          id: userId,
+        },
+        include: {
+          calorieItems: {
+            where: {
+              userId,
+            },
+          },
+          excerciseItems: {
+            where: {
+              userId,
+            },
+          },
+        },
+      });
+
+    return _.omit(user, 'hash');
+  }
+}
